171655fa63cd125d5706fd7c45083e278f64d499
Axe-Blomq <blomqaxe@arcada.fi>
2025-01-29 09:12:45 +0200

    updated env-example

--- app/main.py
+++ app/main.py
@@ -2,21 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#load the wishlist
 @app.get("/")
 def read_root():
     return { "Hello": "Rahti2", "v": "0.4" }
-
-#get info about one specific item
-@app.get("/items/{id}")
-def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
 
 #recieve an item id, take the Token logged user id, and add that to the wishlist
 @app.post("/wishlist/{id}")
 def read_item(item_id: int, q: str = None):
     return {"id": id, "q": q}
 
-#send an item ID to be added to the users cart
-@app.post("/cart/{id}")
+#recieve an item id, take the Token logged user id, and Remove that to the wishlist (if it exists)
+@app.delete("/wishlist/{id}")
 def read_item(item_id: int, q: str = None):
     return {"id": id, "q": q}
